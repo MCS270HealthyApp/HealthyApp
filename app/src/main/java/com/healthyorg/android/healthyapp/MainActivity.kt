530d991858b8dcdc467716deafcb8037a3d21c96
Hamiax <43396618+Hamiax@users.noreply.github.com>
2022-04-01 13:14:49 -0500

    Rudimentary homepage

--- app/src/main/java/com/healthyorg/android/healthyapp/MainActivity.kt
+++ app/src/main/java/com/healthyorg/android/healthyapp/MainActivity.kt
@@ -2,10 +2,44 @@ package com.healthyorg.android.healthyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var summaryButton: Button
+    private lateinit var workoutButton: Button
+    private lateinit var weightButton: Button
+    private lateinit var foodButton: Button
+    private lateinit var notesButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        summaryButton = findViewById(R.id.summary_page_button)
+        workoutButton = findViewById(R.id.workout_page_button)
+        weightButton = findViewById(R.id.weight_page_button)
+        foodButton = findViewById(R.id.food_page_button)
+        notesButton = findViewById(R.id.notes_page_button)
+
+        summaryButton.setOnClickListener{
+            //TODO: goto summary page
+        }
+
+        workoutButton.setOnClickListener{
+            //TODO: goto workout page
+        }
+
+        weightButton.setOnClickListener{
+            //TODO: goto weight page
+        }
+
+        foodButton.setOnClickListener{
+            //TODO: goto food page
+        }
+
+        notesButton.setOnClickListener{
+            //TODO: goto notes page
+        }
     }
 }
