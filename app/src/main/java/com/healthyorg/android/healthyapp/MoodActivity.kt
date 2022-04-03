5ba8bfd8e97940dd3250c854aed91fb0e9b3f9c4
Jasper <jjohns50@gustavus.edu>
2022-04-03 10:49:23 -0500

    Added MoodActivity and created basic layout for mood page

---
+++ app/src/main/java/com/healthyorg/android/healthyapp/MoodActivity.kt
@@ -0,0 +1,11 @@
+package com.healthyorg.android.healthyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mood)
+    }
+}
