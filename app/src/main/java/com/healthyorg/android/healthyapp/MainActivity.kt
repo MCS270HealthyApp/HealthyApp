e9bd42db8edb4de108870526674d9e4ce2401f78
Jasper <jjohns50@gustavus.edu>
2022-04-04 20:59:08 -0500

    Finished quotes page and updated the sleep and mood pages

--- app/src/main/java/com/healthyorg/android/healthyapp/MainActivity.kt
+++ app/src/main/java/com/healthyorg/android/healthyapp/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.healthyorg.android.healthyapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var weightButton: Button
     private lateinit var foodButton: Button
     private lateinit var notesButton: Button
+    private lateinit var sleepButton: Button
+    private lateinit var quotesButton: Button
+    private lateinit var moodButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +25,9 @@ class MainActivity : AppCompatActivity() {
         weightButton = findViewById(R.id.weight_page_button)
         foodButton = findViewById(R.id.food_page_button)
         notesButton = findViewById(R.id.notes_page_button)
+        sleepButton = findViewById(R.id.sleep_page_button)
+        quotesButton = findViewById(R.id.quotes_page_button)
+        moodButton = findViewById(R.id.mood_page_button)
 
         summaryButton.setOnClickListener{
             //TODO: goto summary page
@@ -40,6 +47,21 @@ class MainActivity : AppCompatActivity() {
 
         notesButton.setOnClickListener{
             //TODO: goto notes page
+        }
+
+        sleepButton.setOnClickListener {
+            val intent = Intent(this, SleepActivity::class.java)
+            startActivity(intent)
+        }
+
+        quotesButton.setOnClickListener {
+            val intent = Intent(this, QuotesActivity::class.java)
+            startActivity(intent)
+        }
+
+        moodButton.setOnClickListener {
+            val intent = Intent(this, MoodActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/healthyorg/android/healthyapp/SleepActivity.kt
+++ app/src/main/java/com/healthyorg/android/healthyapp/SleepActivity.kt
@@ -2,10 +2,27 @@ package com.healthyorg.android.healthyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import java.util.Date
 
 class SleepActivity : AppCompatActivity() {
+
+    private lateinit var sleepEditText: EditText
+    private lateinit var sleepEnterButton: Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sleep)
+
+        sleepEditText = findViewById(R.id.sleep_edit_text)
+        sleepEnterButton = findViewById(R.id.sleep_enter_button)
+
+        sleepEnterButton.setOnClickListener {
+            var currentDateVar = Date()
+            var sleepHours = sleepEditText.getText()
+        }
     }
 }
