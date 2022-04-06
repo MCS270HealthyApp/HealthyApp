e8bf87645659ebe25b476e365a749864765fe673
Austin Legatt <Austin-Legatt@users.noreply.github.com>
2022-04-05 21:55:18 -0500

    Rebased

--- app/src/main/java/com/healthyorg/android/healthyapp/FoodActivity.kt
+++ app/src/main/java/com/healthyorg/android/healthyapp/FoodActivity.kt
@@ -1,10 +1,12 @@
 package com.healthyorg.android.healthyapp
 
+import android.content.DialogInterface
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.EditText
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 import androidx.room.Room
@@ -14,7 +16,11 @@ import com.healthyorg.android.healthyapp.foodactivityclasses.FoodListFragment
 import com.healthyorg.android.healthyapp.foodactivityclasses.FoodListViewModel
 import com.healthyorg.android.healthyapp.foodactivityclasses.Meal
 
+private const val TAG = "FoodActivity"
+
 class FoodActivity: AppCompatActivity() {
+    private lateinit var genericFoodButton: Button
+
 
     private lateinit var foodTypeEditText: EditText
     private lateinit var foodCalsEditText: EditText
@@ -27,6 +33,32 @@ class FoodActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food)
+
+        genericFoodButton = findViewById(R.id.generics_list_button)
+
+        //TODO("Replace tempList with a list of food data class objects and generate list based off titles")
+        val tempList: Array<String> = arrayOf("chicken", "apples", "potatoes", "broccoli","chicken", "apples", "potatoes", "broccoli","chicken", "apples", "potatoes", "broccoli","chicken", "apples", "potatoes", "broccoli","chicken", "apples", "potatoes", "broccoli")
+        val boolArray: Array<Boolean> = Array(tempList.size){false}
+
+        genericFoodButton.setOnClickListener{
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+                .setTitle(getString(R.string.generic_food_dialog_title))
+                .setCancelable(true)
+                .setNegativeButton("Close", null)
+                .setPositiveButton("Submit Choice", null)
+                .setMultiChoiceItems(tempList, null){ dialog, which, isChecked ->
+                    boolArray[which] = isChecked
+                }
+            val alertDialog = builder.create()
+            alertDialog.show()
+            val posButton = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)
+            posButton.setOnClickListener{
+                Log.i(TAG, boolArray[0].toString())
+                alertDialog.dismiss()
+                //TODO("Implement addition of selected items to room database")
+                //TODO("Reset boolarray after each submission")
+            }
+        }
 
         foodCalsEditText = findViewById(R.id.food_cals_entry)
         foodTypeEditText = findViewById(R.id.food_type_entry)
