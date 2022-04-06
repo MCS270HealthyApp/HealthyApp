9a10861f6341a425b8b51a768863794336e1bebe
Austin Legatt <Austin-Legatt@users.noreply.github.com>
2022-04-05 22:03:44 -0500

    Implementation modified to reset boolArray between dialog presentations

--- app/src/main/java/com/healthyorg/android/healthyapp/FoodActivity.kt
+++ app/src/main/java/com/healthyorg/android/healthyapp/FoodActivity.kt
@@ -1,9 +1,7 @@
 package com.healthyorg.android.healthyapp
 
-import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -21,7 +19,7 @@ class FoodActivity: AppCompatActivity() {
 
         //TODO("Replace tempList with a list of food data class objects and generate list based off titles")
         val tempList: Array<String> = arrayOf("chicken", "apples", "potatoes", "broccoli","chicken", "apples", "potatoes", "broccoli","chicken", "apples", "potatoes", "broccoli","chicken", "apples", "potatoes", "broccoli","chicken", "apples", "potatoes", "broccoli")
-        val boolArray: Array<Boolean> = Array(tempList.size){false}
+        var boolArray = BooleanArray(tempList.size)
 
         genericFoodButton.setOnClickListener{
             val builder: AlertDialog.Builder = AlertDialog.Builder(this)
@@ -29,7 +27,7 @@ class FoodActivity: AppCompatActivity() {
                 .setCancelable(true)
                 .setNegativeButton("Close", null)
                 .setPositiveButton("Submit Choice", null)
-                .setMultiChoiceItems(tempList, null){ dialog, which, isChecked ->
+                .setMultiChoiceItems(tempList, BooleanArray(tempList.size)){ dialog, which, isChecked ->
                     boolArray[which] = isChecked
                 }
             val alertDialog = builder.create()
@@ -39,7 +37,8 @@ class FoodActivity: AppCompatActivity() {
                 Log.i(TAG, boolArray[0].toString())
                 alertDialog.dismiss()
                 //TODO("Implement addition of selected items to room database")
-                //TODO("Reset boolarray after each submission")
+                boolArray = BooleanArray(tempList.size)
+                alertDialog.dismiss()
             }
         }
     }
