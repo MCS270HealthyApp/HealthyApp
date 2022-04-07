f93fc1bc679704e66589ae74e39c33e609ed1a21
Jasper <jjohns50@gustavus.edu>
2022-04-06 20:21:00 -0500

    Finished the sleep tracker page! This includes the database system!

--- app/src/main/java/com/healthyorg/android/healthyapp/SleepActivity.kt
+++ app/src/main/java/com/healthyorg/android/healthyapp/SleepActivity.kt
@@ -1,28 +1,41 @@
 package com.healthyorg.android.healthyapp
 
+import com.healthyorg.android.healthyapp.database.SleepDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import java.util.Date
+import android.widget.*
+import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
+import com.healthyorg.android.healthyapp.SleepListFragment
 
 class SleepActivity : AppCompatActivity() {
-
+    private lateinit var sleepEntryButton: Button
     private lateinit var sleepEditText: EditText
-    private lateinit var sleepEnterButton: Button
 
-
+    private val sleepListViewModel: SleepListViewModel by lazy {
+        ViewModelProvider(this).get(SleepListViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sleep)
 
+        sleepEntryButton = findViewById(R.id.sleep_enter_button)
         sleepEditText = findViewById(R.id.sleep_edit_text)
-        sleepEnterButton = findViewById(R.id.sleep_enter_button)
 
-        sleepEnterButton.setOnClickListener {
-            var currentDateVar = Date()
-            var sleepHours = sleepEditText.getText()
+        val currentFragment  = supportFragmentManager.findFragmentById(R.id.sleep_fragment_container)
+
+        sleepEntryButton.setOnClickListener{
+            sleepListViewModel.addSleep(DailySleepMood(hours = sleepEditText.text.toString().toDouble()))
         }
+
+        if(currentFragment == null){
+            val fragment = SleepListFragment.newInstance()
+            supportFragmentManager.beginTransaction().add(R.id.sleep_fragment_container, fragment).commit()
+        }
+        val db = Room.databaseBuilder(
+            applicationContext,
+            SleepDatabase::class.java, "database-name"
+        ).build()
     }
 }
