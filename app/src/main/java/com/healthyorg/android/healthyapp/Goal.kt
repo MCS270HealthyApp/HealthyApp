320494bf53ce5fa1d00023f9e4830b34f29cfb29
Aden <amackowick@gustavus.edu>
2022-04-07 00:11:01 -0500

    Everything works Database and Dao created and run without crash.

--- app/src/main/java/com/healthyorg/android/healthyapp/Goal.kt
+++ app/src/main/java/com/healthyorg/android/healthyapp/Goal.kt
@@ -1,6 +1,11 @@
 package com.healthyorg.android.healthyapp
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class Goal(
-    val title: String,
-    var isChecked: Boolean = false
+    @ColumnInfo val title: String,
+    @PrimaryKey var isChecked: Boolean = false
 )
