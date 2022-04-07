8f5cdd2e99defdf4c4b3e9bfd6d830e374056ab0
Aden <amackowick@gustavus.edu>
2022-04-07 00:11:01 -0500

    Everything works Database and Dao created and run without crash.

---
+++ app/src/main/java/com/healthyorg/android/healthyapp/database/GoalDao.kt
@@ -0,0 +1,15 @@
+package com.healthyorg.android.healthyapp.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.healthyorg.android.healthyapp.Goal
+
+@Dao
+interface GoalDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertGoal(vararg goal: Goal)
+
+
+    @Query("SELECT * FROM Goal")
+    fun getAllGoals(): List<Goal>
+}
