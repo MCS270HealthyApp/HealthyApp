9cbf9e86db0976410a2e62bffe8834b5e3ee6cef
Hamiax <43396618+Hamiax@users.noreply.github.com>
2022-04-26 22:16:03 -0500

    added button to delete all data

--- app/src/main/java/com/healthyorg/android/healthyapp/Settings.kt
+++ app/src/main/java/com/healthyorg/android/healthyapp/Settings.kt
@@ -1,19 +1,30 @@
 package com.healthyorg.android.healthyapp
 
+import android.content.DialogInterface
 import android.content.res.Configuration
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.healthyorg.android.healthyapp.MoodClasses.MoodRepository
+import com.healthyorg.android.healthyapp.foodactivityclasses.FoodRepository
+import com.healthyorg.android.healthyapp.goalClasses.GoalsRepository
+import com.healthyorg.android.healthyapp.sleepClasses.SleepRepository
+import com.healthyorg.android.healthyapp.weightClasses.WeightRepository
+import com.healthyorg.android.healthyapp.workoutClasses.WorkoutRepository
+
 
 class Settings : AppCompatActivity() {
     private lateinit var toggleNight: Button
+    private lateinit var resetData: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
         toggleNight = findViewById(R.id.swap_themes_button)
+        resetData = findViewById(R.id.reset_data_button)
 
         toggleNight.setOnClickListener {
             when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
@@ -23,5 +34,58 @@ class Settings : AppCompatActivity() {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
             }
         }
+
+        resetData.setOnClickListener {
+            val alertDialog: AlertDialog? = this.let {
+                val builder = AlertDialog.Builder(it)
+                builder.setMessage(R.string.delete_date_dialog)
+                builder.apply {
+                    setPositiveButton(R.string.confirm_delete,
+                        DialogInterface.OnClickListener { dialog, id ->
+                            deleteAllData()
+                        })
+                    setNegativeButton(R.string.cancel,
+                        DialogInterface.OnClickListener { dialog, id ->
+                            dialog.cancel();
+                        })
+                }
+                builder.create()
+            }
+            alertDialog?.show()
+        }
+    }
+
+    fun deleteAllData() {
+        Thread(Runnable {
+            val workoutRep = WorkoutRepository.get()
+            for (i in workoutRep.getAllWorkoutsAfter(0)) {
+                workoutRep.deleteWorkout(i)
+            }
+
+            val weightRep = WeightRepository.get()
+            for (i in weightRep.getAllWeightsAfter(0)) {
+                weightRep.deleteWeight(i)
+            }
+
+            val sleepRep = SleepRepository.get()
+            for (i in sleepRep.getAllSleepsAfter(0)) {
+                sleepRep.deleteSleep(i)
+            }
+
+            val moodRep = MoodRepository.get()
+            for (i in moodRep.getAllMoodsAfter(0)) {
+                moodRep.deleteMood(i)
+            }
+
+            val goalRep = GoalsRepository.get()
+            for (i in goalRep.getAllGoalsList()) {
+                goalRep.deleteGoal(i)
+            }
+
+            val foodRep = FoodRepository.get()
+            for (i in foodRep.getAllMealsAfter(0)) {
+                foodRep.deleteFood(i)
+            }
+        }).start()
     }
 }
